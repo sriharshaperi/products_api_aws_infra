3bf68bd1866b5e307fcac3df98e9ade63b96c07a
Sri Harsha Peri <peri.s@northeastern.edu>
2023-02-13 21:32:34 -0500

    added initial commit tf_scripts

---
+++ tf_scripts/main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_demo_profile
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = var.cidr_block
+}
+
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.availability_zones)
+  cidr_block        = "${var.subnet_prefix}.${count.index + 1}.${var.subnet_suffix}"
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = var.availability_zones[count.index]
+  tags = {
+    Type = var.public_tag
+    Name = "${var.public_subnet_name}_${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.availability_zones)
+  cidr_block        = "${var.subnet_prefix}.${count.index + 4}.${var.subnet_suffix}"
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = var.availability_zones[count.index]
+  tags = {
+    Type = var.private_tag
+    Name = "${var.private_subnet_name}_${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = var.public_route_table_cidr
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table_association" "public_subnets_association" {
+  count          = length(aws_subnet.public_subnets.*.id)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnets_association" {
+  count          = length(aws_subnet.private_subnets.*.id)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
