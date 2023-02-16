93f7847d90b2155997d7fec3041c3518afad37b6
Sri Harsha Peri <peri.s@northeastern.edu>
2023-02-15 23:23:13 -0500

    tf_script_multiple_vpcs

--- tf_scripts/main.tf
+++ tf_scripts/main.tf
@@ -1,58 +1,140 @@
 provider "aws" {
   region  = var.aws_region
-  profile = var.aws_demo_profile
+  profile = var.aws_profile
 }
 
-resource "aws_vpc" "vpc" {
-  cidr_block = var.cidr_block
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
-resource "aws_subnet" "public_subnets" {
-  count             = length(var.availability_zones)
-  cidr_block        = "${var.subnet_prefix}.${count.index + 1}.${var.subnet_suffix}"
-  vpc_id            = aws_vpc.vpc.id
-  availability_zone = var.availability_zones[count.index]
+resource "aws_vpc" "vpc_1" {
+  cidr_block = var.vpc_cidr[0]
+  tags = {
+    Name = "vpc_1"
+  }
+}
+
+resource "aws_vpc" "vpc_2" {
+  cidr_block = var.vpc_cidr[1]
+  tags = {
+    Name = "vpc_2"
+  }
+}
+
+resource "aws_subnet" "public_subnets_1" {
+  count             = length(data.aws_availability_zones.available.names) > 2 ? 3 : 2
+  cidr_block        = "${var.subnet_prefix_1}.${count.index + 1}.${var.subnet_suffix}"
+  vpc_id            = aws_vpc.vpc_1.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
     Type = var.public_tag
     Name = "${var.public_subnet_name}_${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "private_subnets" {
-  count             = length(var.availability_zones)
-  cidr_block        = "${var.subnet_prefix}.${count.index + 4}.${var.subnet_suffix}"
-  vpc_id            = aws_vpc.vpc.id
-  availability_zone = var.availability_zones[count.index]
+resource "aws_subnet" "public_subnets_2" {
+  count             = length(data.aws_availability_zones.available.names) > 2 ? 3 : 2
+  cidr_block        = "${var.subnet_prefix_2}.${count.index + 1}.${var.subnet_suffix}"
+  vpc_id            = aws_vpc.vpc_2.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Type = var.public_tag
+    Name = "${var.public_subnet_name}_${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets_1" {
+  count             = length(data.aws_availability_zones.available.names) > 2 ? 3 : 2
+  cidr_block        = "${var.subnet_prefix_1}.${count.index + 4}.${var.subnet_suffix}"
+  vpc_id            = aws_vpc.vpc_1.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
     Type = var.private_tag
     Name = "${var.private_subnet_name}_${count.index + 1}"
   }
 }
 
-resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.vpc.id
-}
-
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
-  route {
-    cidr_block = var.public_route_table_cidr
-    gateway_id = aws_internet_gateway.internet_gateway.id
+resource "aws_subnet" "private_subnets_2" {
+  count             = length(data.aws_availability_zones.available.names) > 2 ? 3 : 2
+  cidr_block        = "${var.subnet_prefix_2}.${count.index + 4}.${var.subnet_suffix}"
+  vpc_id            = aws_vpc.vpc_2.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Type = var.private_tag
+    Name = "${var.private_subnet_name}_${count.index + 1}"
   }
 }
 
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_internet_gateway" "internet_gateway_1" {
+  vpc_id = aws_vpc.vpc_1.id
+  tags = {
+    Name = "internet_gateway_1"
+  }
 }
 
-resource "aws_route_table_association" "public_subnets_association" {
-  count          = length(aws_subnet.public_subnets.*.id)
-  subnet_id      = aws_subnet.public_subnets[count.index].id
-  route_table_id = aws_route_table.public_route_table.id
+resource "aws_internet_gateway" "internet_gateway_2" {
+  vpc_id = aws_vpc.vpc_2.id
+  tags = {
+    Name = "internet_gateway_2"
+  }
 }
 
-resource "aws_route_table_association" "private_subnets_association" {
-  count          = length(aws_subnet.private_subnets.*.id)
-  subnet_id      = aws_subnet.private_subnets[count.index].id
-  route_table_id = aws_route_table.private_route_table.id
+resource "aws_route_table" "public_route_table_1" {
+  vpc_id = aws_vpc.vpc_1.id
+  route {
+    cidr_block = var.public_route_table_cidr
+    gateway_id = aws_internet_gateway.internet_gateway_1.id
+  }
+  tags = {
+    Name = "${var.public_tag}_routetable_1"
+  }
+}
+
+resource "aws_route_table" "public_route_table_2" {
+  vpc_id = aws_vpc.vpc_2.id
+  route {
+    cidr_block = var.public_route_table_cidr
+    gateway_id = aws_internet_gateway.internet_gateway_2.id
+  }
+  tags = {
+    Name = "${var.public_tag}_routetable_2"
+  }
+}
+
+resource "aws_route_table" "private_route_table_1" {
+  vpc_id = aws_vpc.vpc_1.id
+  tags = {
+    Name = "${var.private_tag}_routetable_1"
+  }
+}
+
+resource "aws_route_table" "private_route_table_2" {
+  vpc_id = aws_vpc.vpc_2.id
+  tags = {
+    Name = "${var.private_tag}_routetable_2"
+  }
+}
+
+resource "aws_route_table_association" "public_subnets_association_1" {
+  count          = length(aws_subnet.public_subnets_1.*.id)
+  subnet_id      = aws_subnet.public_subnets_1[count.index].id
+  route_table_id = aws_route_table.public_route_table_1.id
+}
+
+resource "aws_route_table_association" "public_subnets_association_2" {
+  count          = length(aws_subnet.public_subnets_2.*.id)
+  subnet_id      = aws_subnet.public_subnets_2[count.index].id
+  route_table_id = aws_route_table.public_route_table_2.id
+}
+
+resource "aws_route_table_association" "private_subnets_association_1" {
+  count          = length(aws_subnet.private_subnets_1.*.id)
+  subnet_id      = aws_subnet.private_subnets_1[count.index].id
+  route_table_id = aws_route_table.private_route_table_1.id
+}
+
+resource "aws_route_table_association" "private_subnets_association_2" {
+  count          = length(aws_subnet.private_subnets_2.*.id)
+  subnet_id      = aws_subnet.private_subnets_2[count.index].id
+  route_table_id = aws_route_table.private_route_table_2.id
 }
--- tf_scripts/variables.tf
+++ tf_scripts/variables.tf
@@ -1,60 +1,54 @@
-variable "availability_zones" {
+variable "vpc_cidr" {
   type        = list(string)
-  description = "List of availability zones to use for subnets."
-  #   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  description = "Mulltiple CIDRs for vpcs in the same region"
 }
 
 variable "aws_region" {
   type        = string
   description = "AWS Region"
-  # default = "us-east-1"
 }
 
-variable "aws_dev_profile" {
-  type = string
-}
-
-variable "aws_demo_profile" {
-  type = string
-}
-
-variable "cidr_block" {
+variable "aws_profile" {
   type        = string
-  description = "CIDR Block for aws_vpc"
-  # default = "10.0.0.0/16"
+  description = "AWS Account profile"
 }
 
 variable "public_tag" {
-  type = string
-  # default = "public"
+  type        = string
+  description = "public tag"
 }
 
 variable "public_subnet_name" {
-  type = string
-  # default = "public_subnet_"
+  type        = string
+  description = "public subnet name"
 }
 
 variable "private_tag" {
-  type = string
-  # default = "private"
+  type        = string
+  description = "private subnet name"
 }
 
 variable "private_subnet_name" {
-  type = string
-  # default = "private_subnet_"
+  type        = string
+  description = "private subnet name"
 }
 
-variable "subnet_prefix" {
-  type = string
-  # default = "10.0."
+variable "subnet_prefix_1" {
+  type        = string
+  description = "subnet prefix for all subnets under vpc 1"
+}
+
+variable "subnet_prefix_2" {
+  type        = string
+  description = "subnet prefix for all subnets under vpc 2"
 }
 
 variable "subnet_suffix" {
-  type = string
-  # default = ".0/24"
+  type        = string
+  description = "subnet suffix for all subnets under all vpcs"
 }
 
 variable "public_route_table_cidr" {
-  type = string
-  # default = "0.0.0.0/0"
+  type        = string
+  description = "public route table CIDR for all ipv4"
 }
