f22d5a722d426ebe1fce4d4a06aa55f9ee3a5aba
Sri Harsha Peri <peri.s@northeastern.edu>
2023-03-31 14:19:34 -0400

    role_policy_cloudwatch

--- tf_scripts/cloudwatch.tf
+++ tf_scripts/cloudwatch.tf
@@ -2,7 +2,7 @@ data "aws_iam_policy" "CloudWatchAgentPolicy" {
   arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "role-ploicy-CW-attach" {
+resource "aws_iam_role_policy_attachment" "role_policy_cloudwatch" {
   role       = aws_iam_role.s3_access_role.name
   policy_arn = data.aws_iam_policy.CloudWatchAgentPolicy.arn
 }
